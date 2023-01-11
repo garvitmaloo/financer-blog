2b5ba8788e44d24b85979cc3b22f833ccb9eb1eb
garvit <garvitmaloo.21@gmail.com>
2023-01-11 16:44:10 +0530

    Made navbar 100% responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,18 @@
 const dropdown = document.querySelector(".dropdown");
-const innerDropdown = document.querySelector(".inner-dropdown");
 const dropdownLink = document.querySelector(".dropdown-link");
-const innerDropdownLink = document.querySelector(".inner-dropdown-link");
 const navSearchIcon = document.querySelector(".nav-search-icon");
 const navSearchFormCloseIcon = document.querySelector(
   ".search-form-close-icon"
 );
 const searchForm = document.querySelector(".search-form");
+const mobileNavCloseIcon = document.querySelector(".mobile-nav-close-icon");
+const body = document.querySelector("body");
+const mobileNavToggleBtn = document.querySelector(".mobile-nav-toggle-btn");
 
 dropdownLink.addEventListener("click", function (e) {
   e.preventDefault();
 
   dropdown.classList.toggle("dropdown-open");
-  innerDropdown.classList.remove("dropdown-open");
-});
-
-innerDropdownLink.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  innerDropdown.classList.toggle("dropdown-open");
 });
 
 navSearchIcon.addEventListener("click", function () {
@@ -27,4 +21,12 @@ navSearchIcon.addEventListener("click", function () {
 
 navSearchFormCloseIcon.addEventListener("click", function () {
   searchForm.classList.remove("show-form");
+});
+
+mobileNavToggleBtn.addEventListener("click", function () {
+  body.classList.add("mobile-nav-active");
+});
+
+mobileNavCloseIcon.addEventListener("click", function () {
+  body.classList.remove("mobile-nav-active");
 });
